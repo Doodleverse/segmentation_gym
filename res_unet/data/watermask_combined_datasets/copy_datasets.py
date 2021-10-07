e0f625d11669aec23749f4202aaf5c3ce41eacec
dbuscombe-usgs <dbuscombe@gmail.com>
2021-10-07 09:56:20 -0700

    interim-commit-donotuse

--- res_unet/data/watermask_combined_datasets/copy_datasets.py
+++
@@ -1,9 +0,0 @@
-
-import os, shutil
-from glob import glob
-
-for file in glob('../watermask_nadir_datasets/*npz'):
-    shutil.copy(file,'./')
-
-for file in glob('../watermask_oblique_datasets/*npz'):
-    shutil.copy(file,'./')
