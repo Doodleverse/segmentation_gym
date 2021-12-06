bc91cd0ccbdb628efcaaa7b070e7c25d218caaa0
dbuscombe-usgs <dbuscombe@gmail.com>
2021-12-05 19:31:15 -0800

    fixed

--- unet/seg_images_in_folder.py
+++ unet/seg_images_in_folder.py
@@ -3,7 +3,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2020, Marda Science LLC
+# Copyright (c) 2020-21, Marda Science LLC
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -23,14 +23,15 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+
 import sys,os, time
 sys.path.insert(1, 'src')
 
-USE_GPU = True #False
+USE_GPU = True
 
 if USE_GPU == True:
    ##use the first available GPU
-   os.environ['CUDA_VISIBLE_DEVICES'] = '1' #'1'
+   os.environ['CUDA_VISIBLE_DEVICES'] = '0' #'1'
 else:
    ## to use the CPU (not recommended):
    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
@@ -42,122 +43,141 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from prediction_imports import *
 #====================================================
 
-#====================================================
 
 root = Tk()
-root.filename =  filedialog.askopenfilename(title = "Select file",filetypes = (("weights file","*.h5"),("all files","*.*")))
-weights = root.filename
-print(weights)
-root.withdraw()
-
-root = Tk()
-root.filename =  filedialog.askdirectory(title = "Select directory of images to segment")
+root.filename =  filedialog.askdirectory(initialdir = "/samples",title = "Select directory of images to segment")
 sample_direc = root.filename
 print(sample_direc)
 root.withdraw()
 
-configfile = weights.replace('.h5','.json').replace('weights', 'config')
+root = Tk()
+root.filename =  filedialog.askopenfilename(title = "Select FIRST weights file",filetypes = (("weights file","*.h5"),("all files","*.*")))
+weights = root.filename
+print(weights)
+root.withdraw()
+
+W=[]
+W.append(weights)
+
+result = 'yes'
+while result == 'yes':
+    result = messagebox.askquestion("More Weights files?", "More Weights files?", icon='warning')
+    if result == 'yes':
+        root = Tk()
+        root.filename =  filedialog.askopenfilename(title = "Select weights file",filetypes = (("weights file","*.h5"),("all files","*.*")))
+        weights = root.filename
+        root.withdraw()
+        W.append(weights)
+
+M= []; C=[]; T = []
+for counter,weights in enumerate(W):
+    configfile = weights.replace('.h5','.json').replace('weights', 'config')
 
 
-with open(configfile) as f:
-    config = json.load(f)
+    with open(configfile) as f:
+        config = json.load(f)
 
-for k in config.keys():
-    exec(k+'=config["'+k+'"]')
+    for k in config.keys():
+        exec(k+'=config["'+k+'"]')
 
 
-from imports import *
+    from imports import *
 
-#=======================================================
+    #=======================================================
 
-print('.....................................')
-print('Creating and compiling model ...')
+    print('.....................................')
+    print('Creating and compiling model {}...'.format(counter))
 
-if MODEL =='resunet':
-    model =  custom_resunet((TARGET_SIZE[0], TARGET_SIZE[1], N_DATA_BANDS),
-                    FILTERS,
-                    nclasses=[NCLASSES+1 if NCLASSES==1 else NCLASSES][0],
-                    kernel_size=(KERNEL,KERNEL),
-                    strides=STRIDE,
+    if MODEL =='resunet':
+        model =  custom_resunet((TARGET_SIZE[0], TARGET_SIZE[1], N_DATA_BANDS),
+                        FILTERS,
+                        nclasses=[NCLASSES+1 if NCLASSES==1 else NCLASSES][0],
+                        kernel_size=(KERNEL,KERNEL),
+                        strides=STRIDE,
+                        dropout=DROPOUT,#0.1,
+                        dropout_change_per_layer=DROPOUT_CHANGE_PER_LAYER,#0.0,
+                        dropout_type=DROPOUT_TYPE,#"standard",
+                        use_dropout_on_upsampling=USE_DROPOUT_ON_UPSAMPLING,#False,
+                        )
+    elif MODEL=='unet':
+        model =  custom_unet((TARGET_SIZE[0], TARGET_SIZE[1], N_DATA_BANDS),
+                        FILTERS,
+                        nclasses=[NCLASSES+1 if NCLASSES==1 else NCLASSES][0],
+                        kernel_size=(KERNEL,KERNEL),
+                        strides=STRIDE,
+                        dropout=DROPOUT,#0.1,
+                        dropout_change_per_layer=DROPOUT_CHANGE_PER_LAYER,#0.0,
+                        dropout_type=DROPOUT_TYPE,#"standard",
+                        use_dropout_on_upsampling=USE_DROPOUT_ON_UPSAMPLING,#False,
+                        )
+
+    elif MODEL =='simple_resunet':
+        # num_filters = 8 # initial filters
+        # model = res_unet((TARGET_SIZE[0], TARGET_SIZE[1], N_DATA_BANDS), num_filters, NCLASSES, (KERNEL_SIZE, KERNEL_SIZE))
+
+        model = simple_resunet((TARGET_SIZE[0], TARGET_SIZE[1], N_DATA_BANDS),
+                    kernel = (2, 2),
+                    num_classes=[NCLASSES+1 if NCLASSES==1 else NCLASSES][0],
+                    activation="relu",
+                    use_batch_norm=True,
                     dropout=DROPOUT,#0.1,
                     dropout_change_per_layer=DROPOUT_CHANGE_PER_LAYER,#0.0,
                     dropout_type=DROPOUT_TYPE,#"standard",
                     use_dropout_on_upsampling=USE_DROPOUT_ON_UPSAMPLING,#False,
-                    )
-elif MODEL=='unet':
-    model =  custom_unet((TARGET_SIZE[0], TARGET_SIZE[1], N_DATA_BANDS),
-                    FILTERS,
-                    nclasses=[NCLASSES+1 if NCLASSES==1 else NCLASSES][0],
-                    kernel_size=(KERNEL,KERNEL),
-                    strides=STRIDE,
+                    filters=FILTERS,#8,
+                    num_layers=4,
+                    strides=(1,1))
+    #346,564
+    elif MODEL=='simple_unet':
+        model = simple_unet((TARGET_SIZE[0], TARGET_SIZE[1], N_DATA_BANDS),
+                    kernel = (2, 2),
+                    num_classes=[NCLASSES+1 if NCLASSES==1 else NCLASSES][0],
+                    activation="relu",
+                    use_batch_norm=True,
                     dropout=DROPOUT,#0.1,
                     dropout_change_per_layer=DROPOUT_CHANGE_PER_LAYER,#0.0,
                     dropout_type=DROPOUT_TYPE,#"standard",
                     use_dropout_on_upsampling=USE_DROPOUT_ON_UPSAMPLING,#False,
-                    )
+                    filters=FILTERS,#8,
+                    num_layers=4,
+                    strides=(1,1))
+    #242,812
 
-elif MODEL =='simple_resunet':
-    # num_filters = 8 # initial filters
-    # model = res_unet((TARGET_SIZE[0], TARGET_SIZE[1], N_DATA_BANDS), num_filters, NCLASSES, (KERNEL_SIZE, KERNEL_SIZE))
+    elif MODEL=='satunet':
+        #model = sat_unet((TARGET_SIZE[0], TARGET_SIZE[1], N_DATA_BANDS), num_classes=NCLASSES)
 
-    model = simple_resunet((TARGET_SIZE[0], TARGET_SIZE[1], N_DATA_BANDS),
-                kernel = (2, 2),
-                num_classes=[NCLASSES+1 if NCLASSES==1 else NCLASSES][0],
-                activation="relu",
-                use_batch_norm=True,
-                dropout=DROPOUT,#0.1,
-                dropout_change_per_layer=DROPOUT_CHANGE_PER_LAYER,#0.0,
-                dropout_type=DROPOUT_TYPE,#"standard",
-                use_dropout_on_upsampling=USE_DROPOUT_ON_UPSAMPLING,#False,
-                filters=FILTERS,#8,
-                num_layers=4,
-                strides=(1,1))
-#346,564
-elif MODEL=='simple_unet':
-    model = simple_unet((TARGET_SIZE[0], TARGET_SIZE[1], N_DATA_BANDS),
-                kernel = (2, 2),
-                num_classes=[NCLASSES+1 if NCLASSES==1 else NCLASSES][0],
-                activation="relu",
-                use_batch_norm=True,
-                dropout=DROPOUT,#0.1,
-                dropout_change_per_layer=DROPOUT_CHANGE_PER_LAYER,#0.0,
-                dropout_type=DROPOUT_TYPE,#"standard",
-                use_dropout_on_upsampling=USE_DROPOUT_ON_UPSAMPLING,#False,
-                filters=FILTERS,#8,
-                num_layers=4,
-                strides=(1,1))
-#242,812
+        model = custom_satunet((TARGET_SIZE[0], TARGET_SIZE[1], N_DATA_BANDS),
+                    kernel = (2, 2),
+                    num_classes=[NCLASSES+1 if NCLASSES==1 else NCLASSES][0],
+                    activation="relu",
+                    use_batch_norm=True,
+                    dropout=DROPOUT,#0.1,
+                    dropout_change_per_layer=DROPOUT_CHANGE_PER_LAYER,#0.0,
+                    dropout_type=DROPOUT_TYPE,#"standard",
+                    use_dropout_on_upsampling=USE_DROPOUT_ON_UPSAMPLING,#False,
+                    filters=FILTERS,#8,
+                    num_layers=4,
+                    strides=(1,1))
 
-elif MODEL=='satunet':
-    #model = sat_unet((TARGET_SIZE[0], TARGET_SIZE[1], N_DATA_BANDS), num_classes=NCLASSES)
-
-    model = custom_satunet((TARGET_SIZE[0], TARGET_SIZE[1], N_DATA_BANDS),
-                kernel = (2, 2),
-                num_classes=[NCLASSES+1 if NCLASSES==1 else NCLASSES][0],
-                activation="relu",
-                use_batch_norm=True,
-                dropout=DROPOUT,#0.1,
-                dropout_change_per_layer=DROPOUT_CHANGE_PER_LAYER,#0.0,
-                dropout_type=DROPOUT_TYPE,#"standard",
-                use_dropout_on_upsampling=USE_DROPOUT_ON_UPSAMPLING,#False,
-                filters=FILTERS,#8,
-                num_layers=4,
-                strides=(1,1))
-
-else:
-    print("Model must be one of 'unet', 'resunet', or 'satunet'")
-    sys.exit(2)
+    else:
+        print("Model must be one of 'unet', 'resunet', or 'satunet'")
+        sys.exit(2)
 
 
-# model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = [mean_iou, dice_coef])
-model.compile(optimizer = 'adam', loss = dice_coef_loss, metrics = [mean_iou, dice_coef])
+    # model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = [mean_iou, dice_coef])
+    model.compile(optimizer = 'adam', loss = dice_coef_loss, metrics = [mean_iou, dice_coef])
 
-model.load_weights(weights)
+    model.load_weights(weights)
+
+    M.append(model)
+    C.append(configfile)
+    T.append(MODEL)
+
 
 metadatadict = {}
-metadatadict['model_weights'] = weights
-metadatadict['config_files'] = configfile
-metadatadict['model_types'] = MODEL
+metadatadict['model_weights'] = W
+metadatadict['config_files'] = C
+metadatadict['model_types'] = T
 
 
 ### predict
@@ -172,14 +192,15 @@ if len(sample_filenames)==0:
 
 print('Number of samples: %i' % (len(sample_filenames)))
 
+temp = -0.05
+
 #look for TTA config
 if not 'TESTTIMEAUG' in locals():
     TESTTIMEAUG = False
 
 for counter,f in enumerate(sample_filenames):
-    do_seg(f, [model], metadatadict, sample_direc,NCLASSES, N_DATA_BANDS,TARGET_SIZE,TESTTIMEAUG)
+    do_seg(f, M, metadatadict, sample_direc,NCLASSES,N_DATA_BANDS,TARGET_SIZE,TESTTIMEAUG, temp)
     print('%i out of %i done'%(counter,len(sample_filenames)))
-<<<<<<< HEAD
 
-=======
->>>>>>> main
+
+# w = Parallel(n_jobs=2, verbose=0, max_nbytes=None)(delayed(do_seg)(f) for f in tqdm(sample_filenames))
