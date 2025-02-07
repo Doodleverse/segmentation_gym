2da4dbe18a0b4094d1252c77376c828df5eb2f84
dbuscombe-usgs <dbuscombe@gmail.com>
2025-02-06 16:36:17 -0800

    make_overlays script

--- utils/make_overlays.py
+++ utils/make_overlays.py
@@ -6,27 +6,25 @@ import matplotlib.pyplot as plt
 import matplotlib 
 from joblib import delayed, Parallel
 import skimage.io as io
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename, askdirectory
+from numpy.lib.npyio import load
+
 
 ##========================================================
 def fromhex(n):
     """ hexadecimal to integer """
     return int(n, base=16)
 
-image_path = r"D:\CDI_runup\AK_runup_timestacks\r2"
-image_path = os.path.normpath(image_path)
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+direc = askdirectory(title='Select directory of results (npz)', initialdir=os.getcwd()+os.sep+'results')
+files = sorted(glob(direc+'/*.npz'))
 
-im_files = sorted(glob(image_path+os.sep+"*.png"))
-len(im_files)
+len(files)
 
+outpath = os.path.dirname(files[0])
 
-# label_path = r"D:\CDI_runup\AK_runup_timestacks\r1\meta"
-# label_path = os.path.normpath(label_path)
-
-# lab_files = sorted(glob(label_path+os.sep+"*.png"))
-# len(lab_files)
-
-lab_files = [i.replace('runup.','runup_predseg.').replace('r2\\','r2\\meta\\') for i in im_files]
-
+### edit below!
 # 0=null, 1=water, 2=sed, 3=veg, 4=wood
 NUM_LABEL_CLASSES = 5
 gr = "#696A6C"
@@ -45,28 +43,30 @@ cmap = matplotlib.colors.ListedColormap(class_label_colormap[:NUM_LABEL_CLASSES]
 cmap2 = matplotlib.colors.ListedColormap(['#000000']+class_label_colormap[:NUM_LABEL_CLASSES])
 
 
-def do_it(im, lab, cmap):
+def do_it(file, outpath, cmap):
     try:
-        l = io.imread(lab) 
-        l[l==3] = 0
+
+        data = dict()
+        with load(file, allow_pickle=True) as dat:
+            #create a dictionary of variables
+            #automatically converted the keys in the npz file, dat to keys in the dictionary, data, then assigns the arrays to data
+            for k in dat.keys():
+                data[k] = dat[k]
+            del dat
+
+        infile = str(data['input_file'])
+        outfile = infile.split(os.sep)[-1].replace('.jpg','_overlay.png')
+
+        im = io.imread(infile)
         fig = plt.figure(figsize=(6,6))
-        plt.imshow(io.imread(im))
-        plt.imshow(l, cmap=cmap, alpha=0.5, vmin=0, vmax=NUM_LABEL_CLASSES)
+        plt.imshow(im)
+        plt.imshow(data['grey_label'], cmap=cmap, alpha=0.5, vmin=0, vmax=NUM_LABEL_CLASSES)
         # plt.show()
-        plt.savefig(im.replace("runup.","overlay.").replace('.png','.jpg'), dpi=300, bbox_inches='tight')
+        plt.savefig(outpath+os.sep+outfile, dpi=300, bbox_inches='tight')
         plt.close('all')
     except:
-        pass
+        # pass
+        print(f"{file} failed")
 
-# for im,lab in zip(im_files,lab_files):
-    
-#     l = io.imread(lab) 
-#     l[l==3] = 0
-#     fig = plt.figure(figsize=(6,6))
-#     plt.imshow(io.imread(im))
-#     plt.imshow(l, cmap=cmap, alpha=0.5, vmin=0, vmax=NUM_LABEL_CLASSES)
-#     # plt.show()
-#     plt.savefig(im.replace("images","overlays").replace('.tif','.png'), dpi=300, bbox_inches='tight')
-#     plt.close('all')
 
-Parallel(n_jobs=-2)(delayed(do_it)(i,l,cmap) for i,l in zip(im_files,lab_files))
+Parallel(n_jobs=-2)(delayed(do_it)(i,outpath,cmap) for i in files)
